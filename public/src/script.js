ada934d43cd386fb05fcc94e9108933482d2982d
kopisusu <komangwluce@gmail.com>
2015-04-07 18:31:27 -0400

    fixed fade in

--- public/src/script.js
+++ public/src/script.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
   $(window).scroll(function() {
     s = $(window).scrollTop();
 
-    if (s > 702) {
-      $('.switch a').css('position', 'fixed').css('right', '13px');
+    if (s > 702 && s < 1300 ) {
+      $('.switch a').css('position', 'fixed').css('right', '13px').css('opacity', 1);
       $('.landing .side-bar .side p ').css('opacity', '0');
+    } else if (s > 1300) {
+      $('.switch a').css('opacity', 0);
     } else {
-      $('.switch a').css('position', 'relative').css('right', '0px');
+      $('.switch a').css('position', 'relative').css('right', '0px').css('opacity', 0);
       $('.landing .side-bar .side p ').css('opacity', '1');
     };
   });
